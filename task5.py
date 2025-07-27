b5845cc1456eb541f57f44c43593d5d21b42daf4
praveencs-dev <praveensculpture@gmail.com>
2025-07-27 14:24:24 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,5 +1,6 @@
 from  flask import Flask,render_template
 from flask_socketio import SocketIO,emit
+import os
 app=Flask(__name__)
 socketio=SocketIO(app)
 @app.route('/')
@@ -12,8 +13,6 @@ def handle_send_message(data):
     print(f"{username}:{message}")
     emit('recievemessage',{'username':username,'message':message},broadcast=True)
 if __name__=='__main__':
-    socketio.run(app,debug=True)
-import os
-port=int(os.environ.get("PORT",5000))
-socketio.run(app,host='0.0.0.0',port=port)
+     port=int(os.environ.get('PORT',5000))
+     socketio.run(app,host='0.0.0.0',port=port)
 
