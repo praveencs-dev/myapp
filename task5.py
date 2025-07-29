88ee86bf9c96cb881d60c34badaabda14d9944c4
praveencs-dev <praveensculpture@gmail.com>
2025-07-29 12:48:26 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,16 @@
+from  flask import Flask,render_template
+from flask_socketio import SocketIO,emit
+app=Flask(__name__)
+socketio=SocketIO(app)
+@app.route('/')
+def index():
+     return render_template('index.html')
+@socketio.on('sendmessage')
+def handle_send_message(data):
+    username=data['username']
+    message=data['message']
+    time=data['time']
+    print(f"{username}:{message}:{time}")
+    emit('recievemessage',{'username':username,'message':message,'time':time},broadcast=True)
+if __name__=='__main__':
+    socketio.run(app,debug=True)
